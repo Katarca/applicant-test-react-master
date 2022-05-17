ed897d422d00561daa0680f95c9a304737f29ec6
Katarca <souskovakatarina@gmail.com>
2022-05-17 12:45:24 +0200

    persisting cart in local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./";
 import { MainArea, StyledApp } from "./page-style/StyledApp";
@@ -8,9 +8,16 @@ import Home from "./components/pages/Home";
 import Cart from "./components/pages/Cart";
 import CartContext from "./helpers/CartContext";
 
+const cartLocalStorage = JSON.parse(localStorage.getItem("cart") || []);
+
 function App() {
   const products = data;
-  const [cartItems, setCartItems] = useState([]);
+
+  const [cartItems, setCartItems] = useState(cartLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const addToCart = (event, product, number, setNumber) => {
     const itemExist = cartItems.find((item) => item.id === product.id);
